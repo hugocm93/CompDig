ea8cf7adca2ce7888dafd055ce75ccd9ffbdb8c3
Marcos Boekel <marcosboekel@gmail.com>
2017-06-02 10:17:03 -0300

    CriacaoVHDL

---
+++ CPU/Lab04/CPU.vhd
@@ -0,0 +1,26 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity CPU is
+    Port ( RESET : in  STD_LOGIC;
+           LoadPC : in  STD_LOGIC;
+           EnMEM : in  STD_LOGIC;
+           clkAC : in  STD_LOGIC;
+           EnPC : in  STD_LOGIC;
+           clkIR : in  STD_LOGIC;
+           IncPC : in  STD_LOGIC;
+           EnMBR : in  STD_LOGIC;
+           clkMBR : in  STD_LOGIC;
+           EnALU : in  STD_LOGIC;
+           RESETAC : in  STD_LOGIC);
+end CPU;
+
+architecture Behavioral of CPU is
+
+begin
+
+
+end Behavioral;
+
