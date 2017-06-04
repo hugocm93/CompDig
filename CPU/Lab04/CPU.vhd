ca00119df45fb465e7f365d0fc88f0094b18332b
Hugo Machado <hugocm93@gmail.com>
2017-06-04 17:55:21 -0300

    cpu

--- CPU/Lab04/CPU.vhd
+++ CPU/Lab04/CPU.vhd
@@ -42,9 +42,89 @@ architecture Behavioral of CPU is
         ,BRA_T1_0
         ,BRA_T1_1
     );
-    signal CPU_STATE is state;
+signal CPU_STATE is state;
+signal addr_bus, data_bus : 
+        std_logic_vector(7 downto 0) := (others => '0');
+signal auxMBR, auxPC, auxAC, auxALU, auxData: 
+        std_logic_vector(7 downto 0) := (others => '0');
 
 begin
+
+    RAM_TRI :  entity work.tri_state_buffer(Behavioral) PORT MAP(
+        IN8 => auxData,
+        OE => EnMEM,
+        OUT8 => data_bus 
+    );
+
+    ALU_TRI :  entity work.tri_state_buffer(Behavioral) PORT MAP(
+        IN8 => auxALU,
+        OE => EnALU,
+        OUT8 => addr_bus 
+    );
+
+    MBR_TRI :  entity work.tri_state_buffer(Behavioral) PORT MAP(
+        IN8 => auxMBR,
+        OE => EnMBR,
+        OUT8 => data_bus 
+    );
+
+    PC_TRI :  entity work.tri_state_buffer(Behavioral) PORT MAP(
+        IN8 => auxPC,
+        OE => EnPC,
+        OUT8 => data_bus 
+    );
+
+    IR : entity work.register8(Behavioral) PORT MAP(
+        D   => data_bus,
+        MReset => reset,
+        CP => clkIR,
+        Q   => '0' -- nao usado por enquanto
+    );
+
+    MBR : entity work.register8(Behavioral) PORT MAP(
+        D   => data_bus,
+        MReset => reset,
+        CP => clkMBR,
+        Q   => auxMBR 
+    );
+
+    AC : entity work.register8(Behavioral) PORT MAP(
+        D   => addr_bus,
+        MReset => reset and RESETAC,
+        CP => clkAC,
+        Q   => auxAC 
+    );
+
+    PC : entity work.up_counter_load(rtl) PORT MAP(
+        cout   => auxPC,
+        data   => data_bus,
+        load   => LoadPC,
+        enable => '1', -- habilitado
+        clk    => IncPC,
+        reset  => reset 
+    );
+
+    ALU : entity work.alu8bit(behavioral) PORT MAP(
+        a => auxAC,
+        b => data_bus,
+        f => auxALU, 
+        zero => '0', --nao usado por enquanto
+        op => "010" --soma por enquanto
+    );
+
+    RAM : entity work.SingleRAM_syn(ram_arch)
+        GENERIC MAP(
+            ADDR_WIDTH => 7,
+            DATA_WIDTH => 7
+        );
+        PORT MAP(
+            clk => clk,
+            we => '0', --leitura 
+            addr => addr_bus,
+            din => auxData,
+            dout => auxData
+    );
+
     process(reset, clk) begin
     end process;
 
